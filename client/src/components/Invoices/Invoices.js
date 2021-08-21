d08d6ec9f4a98962af8f38bad8c5d7cb4b5bb9ab
mohamednasr20 <mohamednasr8687@gmail.com>
2021-08-21 04:46:11 +0200

    add incoices styles

--- client/src/components/Invoices/Invoices.js
+++ client/src/components/Invoices/Invoices.js
@@ -3,9 +3,11 @@ import Invoice from './Invoice/Invoice';
 import InvoicesNav from './InvoicesNav/InvoicesNav';
 import EmptyInvoicesList from './EmptyInvoicesList/EmptyInvoicesList';
 import { useSelector } from 'react-redux';
+import useStyles from './styles';
 
 const Invoices = () => {
   const invoices = useSelector((state) => state.GlobalState.invoices);
+  const classes = useStyles();
   const [status, setStatus] = useState('');
 
   const handleChangeStatus = (e) => {
@@ -18,7 +20,7 @@ const Invoices = () => {
       : invoices.filter((invoice) => invoice.status === status);
 
   const itemsList = currentInvoices.length ? (
-    <div>
+    <div className={classes.itemsList}>
       {currentInvoices.map((invoice) => (
         <Invoice key={invoice._id} invoice={invoice} />
       ))}
@@ -28,7 +30,7 @@ const Invoices = () => {
   );
 
   return (
-    <div>
+    <div className={classes.root}>
       <InvoicesNav
         invoices={currentInvoices}
         status={status}
---
+++ client/src/components/Invoices/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    paddingTop: 40,
+  },
+  itemsList: {
+    marginTop: 50,
+  },
+}));
+
+export default useStyles;

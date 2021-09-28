c9e55ff0e0dc6c188c077018dd883ecfee7ea972
mohamednasr20 <mohamednasr8687@gmail.com>
2021-09-27 21:52:26 -0400

    add auth middleware

--- server/routes/invoices.js
+++ server/routes/invoices.js
@@ -7,13 +7,15 @@ import {
   updateInvoice,
   invoiceStatus,
 } from '../controllers/invoices.js';
+import auth from '../middleware/auth';
+
 const router = express.Router();
 
 router.get('/', getInvoices);
 router.get('/:id', getInvoice);
-router.post('/', createInvoice);
-router.put('/:id', updateInvoice);
-router.delete('/:id', deleteInvocie);
-router.patch('/:id/invoiceStatus', invoiceStatus);
+router.post('/', auth, createInvoice);
+router.put('/:id', auth, updateInvoice);
+router.delete('/:id', auth, deleteInvocie);
+router.patch('/:id/invoiceStatus', auth, invoiceStatus);
 
 export default router;
